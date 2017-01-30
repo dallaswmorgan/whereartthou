b84506cda428a9addc4fac41fbf215cdebede576
Dallas Morgan <dallaswmorgan@gmail.com>
2017-01-30 10:17:04 -0800

    Added react-native framework

---
+++ index.ios.js
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class WhereArtThou extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          We have an app!
+        </Text>
+        <Text style={styles.instructions}>
+          Look at all this cool stuff we can do
+        </Text>
+        <Text style={styles.instructions}>
+          So many cool things
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('WhereArtThou', () => WhereArtThou);
