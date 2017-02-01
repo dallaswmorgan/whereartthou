23e4f0aa24753a8dff82d510f5ddbba52fcc9b21
stmosher27 <stmosher27@gmail.com>
2017-02-01 14:21:04 -0800

    Export stylesheet

--- app/components/map.js
+++ app/components/map.js
@@ -180,38 +180,4 @@ PolygonCreator.propTypes = {
   provider: MapView.ProviderPropType,
 };
 
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    width: width - 10,
-    height: 300,
-    marginLeft: 5,
-    marginTop: 100
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  bubble: {
-    backgroundColor: 'rgba(255,255,255,0.7)',
-    paddingHorizontal: 18,
-    paddingVertical: 12,
-    borderRadius: 20,
-  },
-  latlng: {
-    width: 200,
-    alignItems: 'stretch',
-  },
-  button: {
-    width: 80,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    marginVertical: 20,
-    backgroundColor: 'transparent',
-  },
-});
-
 module.exports = PolygonCreator;
