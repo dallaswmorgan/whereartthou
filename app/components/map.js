e5b901ab67abf116b99c2909795a21ebb17eda9f
Dallas Morgan <dallaswmorgan@gmail.com>
2017-01-31 23:47:12 -0800

    Polygon rendering

--- app/components/map.js
+++ app/components/map.js
@@ -34,8 +34,13 @@ export default class Map extends Component {
               longitude: -122.4324,
               latitudeDelta: 0.0922,
               longitudeDelta: 0.0421,
-            }}
-          />
+            }}>
+             <MapView.Polygon
+              coordinates={[
+                {latitude: 37.6, longitude: -122.4},
+                {latitude: 37.8, longitude: -122.4},
+                {latitude: 37.7, longitude: -122.5}]}/>
+          </MapView>
         </View>
     );
 
